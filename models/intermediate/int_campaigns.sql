b59f48e50ff502539948a08b0019571c3ce990aa
Christopher Tavares <christavares33@gmail.com>
2024-02-07 15:28:51 +0000

    setup livecode

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,3 +1,11 @@
-{{ dbt_utils.union_relations(
-    relations=[ref("stg_raw__raw_gz_adwords"), ref("stg_raw__raw_gz_facebook"), ref("stg_raw__raw_gz_bing"), ref("stg_raw__raw_gz_criteo")]
-) }}
+SELECT *
+FROM {{ref('stg_raw__raw_gz_adwords')}}
+UNION ALL 
+SELECT *
+FROM {{ref('stg_raw__raw_gz_bing')}}
+UNION ALL 
+SELECT *
+FROM {{ref('stg_raw__raw_gz_criteo')}}
+UNION ALL 
+SELECT *
+FROM {{ref('stg_raw__raw_gz_facebook')}}
