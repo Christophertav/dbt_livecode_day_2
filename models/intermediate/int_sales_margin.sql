63a945f4bebb917f531157344f37547a281a0c44
Christopher Tavares <christavares33@gmail.com>
2024-02-07 16:48:34 +0000

    final changes livecode

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,18 @@
-SELECT 
-    products_id, 
-    date_date, 
-    orders_id,
-    revenue, 
-    quantity, 
-    CAST(purchase_price AS FLOAT64), 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p 
-    USING (products_id)
+WITH margin_table AS(
+    SELECT 
+        products_id, 
+        date_date, 
+        orders_id,
+        revenue, 
+        quantity, 
+        CAST(purchase_price AS FLOAT64) AS purchase_price,
+        ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+        s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+        
+    FROM {{ref("stg_raw__sales")}} s
+    LEFT JOIN {{ref("stg_raw__product")}} p 
+        USING (products_id)
+)
+SELECT *
+    ,{{margin_percent(revenue,purchase_cost)}} AS margin_percent
+FROM margin_table
